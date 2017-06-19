7e853b474867639147486cebd908f970202c5c23
dvojtise <didier.vojtisek@inria.fr>
2017-06-19 17:29:37 +0200

    removing code not related to exploration

--- moccml/plugins/editors/textual/org.eclipse.gemoc.moccml.constraint.ccslmocc.model.xtext.mocdsl.ui/src/org/eclipse/gemoc/moccml/constraint/ccslmocc/model/xtext/ui/wizard/MoCDslProjectInfo.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.gemoc.moccml.constraint.ccslmocc.model.xtext.ui.wizard;
-
-public class MoCDslProjectInfo extends org.eclipse.xtext.ui.wizard.DefaultProjectInfo {
-	
-}
--- moccml/plugins/editors/textual/org.eclipse.gemoc.moccml.constraint.fsmkernel.model.xtext.fsmdsl.ui/src/org/eclipse/gemoc/moccml/constraint/fsmkernel/model/xtext/ui/wizard/FSMDslProjectInfo.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.gemoc.moccml.constraint.fsmkernel.model.xtext.ui.wizard;
-
-public class FSMDslProjectInfo extends org.eclipse.xtext.ui.wizard.DefaultProjectInfo {
-	
-}
